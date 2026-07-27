f0111a2aa62aa3cc8d1f0d05c0114e953962bfdc
k1llbk <bida24-353@thuto.bac.ac.bw>
2026-07-27 15:09:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,11 +25,16 @@ window.addEventListener("scroll", () => {
    HAMBURGER MENU
 ===================================== */
 
-const menuToggle = document.getElementById("menuToggle");
-const navMenu = document.getElementById("navMenu");
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-menuToggle?.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
+
+hamburger?.addEventListener("click", () => {
+
+    hamburger.classList.toggle("active");
+
+    navLinks.classList.toggle("active");
+
 });
 
    /* =====================================
